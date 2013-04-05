801df058c1bc10c77af07f7a1e395a657e224190
Tyler <underlost@gmail.com>
2013-04-04 21:36:04 -0500

    Updating django

--- bloop/accounts/forms.py
+++ bloop/accounts/forms.py
@@ -1,6 +1,7 @@
-from __future__ import absolute_import
 from django import forms
-from registration.forms import RegistrationFormUniqueEmail
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+from django.contrib.auth.models import User
 from .models import Profile
 
 class RegistrationForm(RegistrationFormUniqueEmail):
@@ -33,3 +34,34 @@ class ProfileForm(forms.ModelForm):
             if commit:
                 instance.user.save()
         return instance
+
+
+class UserCreationForm(forms.ModelForm):
+    error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+    }
+    username = forms.RegexField(label=_("Username"), max_length=30,
+        regex=r'^[\w-]+$')
+    password = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
